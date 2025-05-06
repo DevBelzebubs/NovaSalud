78184671cf92837a6635d8f73b4116e04e6c1519
Juan Diego <juandiego.gr05@gmail.com>
2025-05-05 22:47:36 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopBarComponent } from "./top-bar/top-bar.component";
+import { MainContentComponent } from "./main-content/main-content.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopBarComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
